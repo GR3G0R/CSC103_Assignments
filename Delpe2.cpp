effb76474d7163308e459a200bb0380839359899
Gregory Delpe <gregory@namely.com>
2016-09-24 23:32:24 -0400

    Implement score logic

--- Delpe2.cpp
+++ Delpe2.cpp
@@ -6,39 +6,45 @@
 #include <ctime>
 #include <cstdlib>
 #include <cassert>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-static const char a_car[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'\0'";
-static const char car[] = "*&^%$#@!'\0'";
-static const char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'\0'";
-static const char num[] = "0123456789'\0'";
+static const char a_car[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+static const char car[] = "*&^%$#@!";
+static const char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+static const char num[] = "0123456789";
 //static const char alphanum[] = "0123456789" "!@#$%^&*" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz";
 
-char genRandom() {
+char RNG() {
     int stringLength = sizeof(a_car) -1;
     return a_car[rand() % stringLength];
 }
 
 int main() {
 srand(time(0));
-int mac_allow = 1000, man_allow = 1000;
+int man_allow = 1000;
 
-//while (true) {
-  cout << "Your current points 1000, just type -> ";
+while (true) {
+
+cout << "Your current points " << man_allow << ", just type -> ";
 
 
-or (int a = 1; a<=7;++a) {
+char ranCar = RNG();
+
+for (int a = 1; a<=7;++a) {
 for (int i = 0;i<sizeof(a_car)-1;++i) {
-    char g = genRandom();
-  if (g  == num[i]) {
+  char ranCar = RNG();
+  if (ranCar  == num[i]) {
     cout << "[0-9]";
     break;
   }
-  else if (g  == alpha[i]) {
-    cout << g;
+  else if (ranCar  == alpha[i]) {
+    cout << ranCar;
     break;
   }
-  else if (g == car[i]) {
+  else if (ranCar == car[i]) {
     cout << "[%-&]";
     break;
   }
@@ -46,19 +52,55 @@ for (int i = 0;i<sizeof(a_car)-1;++i) {
     continue;
   }
  }
+}
 
+cout <<  ": ";
+
+char usr_input[6];
+
+chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+cin >> usr_input;
+chrono::steady_clock::time_point end= chrono::steady_clock::now();
+int inSpeed = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
+
+
+char ranCar_collect[6];
+for (int i = 0; i < sizeof(ranCar); ++i) {
+ranCar_collect[i] = ranCar;
+break;
+}
+
+if (inSpeed >= 7000) {
+  man_allow -= (inSpeed - 7000);
+  cout << inSpeed << " milliseconds, you did not make it within the interval of 7000..."
+    << endl << endl;
+  continue;
+}
+else if (inSpeed <= 7000) {
+  for (int i = 0; i<=6;++i){
+    for (int j = 0; i<=6;++j){
+    if (ranCar_collect[i] == usr_input[j]) {
+     man_allow += 500;
+     break;
+    }
+    else if (ranCar_collect[i] != usr_input[j]) {
+    int offset = ranCar_collect[i] - usr_input[j];
+    man_allow -= offset;
+    break;
+    }
+    else {
+    break;
+    }
+  }
+}
+  cout << inSpeed << " millisecond, you made it within the interval of 7000..."
+    << endl << endl;
+  continue;
+}
+else {
+continue;
+}
 
 }
 
-#if 0
-//<<  ": ";
-
-//string usr_input;
-
-//clock_t begin = clock();
-/*chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-cin >> usr_input;
-chrono::steady_clock::time_point end= chrono::steady_clock::now();
-cout << "Time difference = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << endl;*/
-#endif
 }
