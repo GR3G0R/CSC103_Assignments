9a5bc731b837dd012151c6db1f8cedaf176e2dcb
Gregory Delpe <gregory@namely.com>
2016-10-02 22:34:44 -0400

    Shuffle user input

--- Delpe3.cpp
+++ Delpe3.cpp
@@ -1,9 +1,10 @@
-//Gregory Delpe 
+//Gregory Delpe
 //Assignment 3: Animal Quz
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 using namespace std;
 
 
@@ -26,37 +27,42 @@ vector<string> pushtoken (string l) { //Fucntion pushes user input to vector str
   return tok;
 }
 
-/*void game_init () {
-  shuffle(tokens.begin(),tokens.end());
-  int num_animals = uniform_int_distribution <int> (1,3) rand();
-  for (int i = 0; i < num_animals; i++) choice.push_back(i);
-  shuffle(choice.begin(),choice.end());
-  
-}*/
+// void game_init () {
+//   shuffle(tokens.begin(),tokens.end());
+//   int num_animals = uniform_int_distribution <int> (1,3) rand();
+//   for (int i = 0; i < num_animals; i++) choice.push_back(i);
+//   shuffle(choice.begin(),choice.end());
+//
+// }
 
 int main() {
-  vector <string> tokens;
-  string usr_input;
-  string guess_word;
+    srand(time(0));
+    vector <string> tokens;
+    string usr_input;
+    string guess_word;
 
-  cout << "Enter at least five animal names, e.g., cat, dog, etc..." << endl;
-  while (true) {
+    bool inputting_animals = true;
+    cout << "Enter at least five animal names, e.g., cat, dog, etc..." << endl;
+    while (true) {
         cout << "> ";
         getline(cin, usr_input); //Get user input
         if (usr_input == "") break;
-
         vector<string> tok = pushtoken(usr_input); //Assign returned user input to local vector
-
+        // TODO assert vector is at least five elements
         tokens.insert(tokens.end(), tok.begin(), tok.end());  //Push returned user input onto stack
-
         for (int i = 0; i < tokens.size(); ++i) {  //Copy out user input
             cout << i + 1 << ": " << tokens[i] << endl;
         }
 
-        //for (int i = 0, i < tokens.size(); ++i) {
-             
-        //}
-  }
+        random_shuffle(tokens.begin(), tokens.end());
+        int num_animals = rand() % 3 + 1; // num_animals in the range 1 to 3
+        vector <string> challenges;
+        for (int i = 0; i < num_animals; i++) challenges.push_back(tokens[i]);
+        string challenge;
+        for_each(challenges.begin(), challenges.end(), [&](string &piece){ challenge += piece; });
+        random_shuffle(challenge.begin(), challenge.end());
+        cout << challenge << endl;
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,9 @@
+To compile:
+```bash
+clang++ -o output -std=c++11 Delpe3.cpp
+```
+
+To run:
+```bash
+./output
+```
