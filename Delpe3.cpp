c14c300eeb310444c69d821069f34feadc186f1a
Gregory Delpe <gregory@namely.com>
2016-10-03 22:14:57 -0400

    Implement function scramble

--- Delpe3.cpp
+++ Delpe3.cpp
@@ -5,26 +5,27 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <cassert>
 using namespace std;
 
 
-vector<string> pushtoken (string l) { //Fucntion pushes user input to vector string object for collection
-  vector<string> tok;
-  string token;
-  bool found = false;
-  for (int i = 0; i < l.length(); ++i) {
-      if (!isspace(l[i])) { //If user input is not a blank space push string elements onto stack
-          token += l[i];
-          found = true;
-          continue;
-      }
-      if (!found) continue; //If blank space is detected return to loop
-      tok.push_back(token); //End of word reached -> stack padded with spaces
-      token = "";
-      found = false;
-  }
-  if (found) tok.push_back(token);  //If word detected -> pushed onto stack
-  return tok;
+vector<string> pushtoken (string l) {  //Fucntion pushes user input to vector string object for collection
+    vector<string> tok;
+    string token;
+    bool found = false;
+    for (int i = 0; i < l.length(); ++i) {
+        if (!isspace(l[i])) {  //If user input is not a blank space push string elements onto stack
+            token += l[i];
+            found = true;
+            continue;
+        }
+        if (!found) continue;  //If blank space is detected return to loop
+        tok.push_back(token);  //End of word reached -> stack padded with spaces
+        token = "";
+        found = false;
+    }
+    if (found) tok.push_back(token);      //If word detected -> pushed onto stack
+    return tok;
 }
 
 // void game_init () {
@@ -41,6 +42,17 @@ void print_animals(vector <string> animals) {
     }
 }
 
+string scramble (vector <string> capture){  //Generate randomly shuffled string
+    string challenge;
+    vector <string> challenges;
+    random_shuffle(capture.begin(), capture.end());
+    int num_animals = rand() % 3 + 1;  // num_animals in the range 1 to 3
+    for (int i = 0; i < num_animals; i++) challenges.push_back(capture[i]);
+    for_each(challenges.begin(), challenges.end(), [&](string &piece){ challenge += piece; });  //Construct string from vector
+    random_shuffle(challenge.begin(), challenge.end());  //Scramble string
+    return challenge;
+}
+
 int main() {
     srand(time(0));
     vector <string> tokens;
@@ -49,25 +61,23 @@ int main() {
 
     cout << "Enter at least five animal names, e.g., cat, dog, etc..." << endl;
     while (true) {
+        // if (usr_input == "?") {
+        //     print_animals(tokens);
+        //     usr_input = "";
+        //     continue;
+        // }
         cout << "> ";
         getline(cin, usr_input); //Get user input
-        if (usr_input == "") break; // QUIT
-
         vector<string> tok = pushtoken(usr_input); //Assign returned user input to local vector
-        // TODO assert vector is at least five elements
         tokens.insert(tokens.end(), tok.begin(), tok.end());  //Push returned user input onto stack
+        assert (tokens.size() >= 5);  //Assert vector is at least five elements
+        guess_word = scramble(tokens);
+        if (usr_input == "") break;  // QUIT
         print_animals(tokens);
 
-        random_shuffle(tokens.begin(), tokens.end());
-        int num_animals = rand() % 3 + 1; // num_animals in the range 1 to 3
-        vector <string> challenges;
-        for (int i = 0; i < num_animals; i++) challenges.push_back(tokens[i]);
-        string challenge;
-        for_each(challenges.begin(), challenges.end(), [&](string &piece){ challenge += piece; });
-        random_shuffle(challenge.begin(), challenge.end());
-        cout << challenge << endl;
     }
-
+    
+    cout << guess_word << endl;
 }
 
 
