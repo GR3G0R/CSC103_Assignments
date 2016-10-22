65667ebf49aacd783a1cbc7ba0de46f444b36921
Gregory Delpe <gregory@namely.com>
2016-10-22 16:37:42 -0400

    Implement reveal statement for passed card

--- Delpe4.cpp
+++ Delpe4.cpp
@@ -13,7 +13,8 @@ class Card {  //Introduce Card Class
 public:
     Card(const char s = 'S', const int n = 0) : m_char(s), m_num(n) {}  //Card class constructor
     friend ostream& operator<<(ostream&, const Card&);
-
+    friend bool operator==(const Card& c, const Card& d);
+    friend bool operator!=(const Card& a, const Card& b);
 };
 ostream& operator<<(ostream& o, const Card& c) {  //Overload extraction operator to produce card value
     o << c.m_char;
@@ -27,9 +28,20 @@ ostream& operator<<(ostream& o, const Card& c) {  //Overload extraction operator
     return o;
 }
 
+bool operator==(const Card& a, const Card& b) {
+    return a == b;
+
+}
+
+bool operator!=(const Card& c, const Card& d) {
+    return !(c == d);
+}
+
+
+
 class Player {  //Introduce Player class
     string m_name;
-    vector <Card> m_deck;
+    vector<Card> m_deck;
 public:
     vector <char> letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
 
@@ -37,11 +49,14 @@ public:
     void push_back(const Card& c) {m_deck.push_back(c); }
     friend ostream& operator<<(ostream&, const Player& p);
     // Card get_card(int index) { return m_deck[index]; }
-    Card pass_card(int index) {
+    friend bool operator==(const Player& c, const Player& d);
+    friend bool operator!=(const Player& a, const Player& b);
+    Card pass_card(int index) {  //Returns card to be removed and removes card from player deck
         Card result = m_deck[index];
-        m_deck.erase(m_deck.begin() + index);
-        return result;
+        m_deck.erase(m_deck.begin() + index); return result;
     }
+    string get_name() const { return m_name; }
+    vector<Card> get_hand() const { return m_deck; }
 };
 // ostream& operator<<(ostream& o, const Player& p) {  //Extraction operator overloaded to print player name and hand
 //     o << "Player: " << p.m_name << " has ";
@@ -51,13 +66,33 @@ public:
 //     return o;
 // }
 ostream& operator<<(ostream& o, const Player& p) {  //Extraction operator overloaded to print player name and hand
-    o << p.m_name << " ";
+    //o << p.m_name << " ";
     for (int i = 0, len = p.m_deck.size(); i < len; ++i) {
         o << "(" << p.letters[i] << ") " << p.m_deck[i] << " ";
     }
     return o;
 }
 
+bool operator==(const Player& a, const Player& b) {
+    return a.get_hand() == b.get_hand();
+
+}
+
+bool operator!=(const Player& c, const Player& d) {
+    return !(c == d);
+}
+
+/*class Hint {
+   Player p_name;
+   Card passed_card;
+   int index;
+public:
+    Hint(const Card c, const Player = p) passed_card(c), p_name(p) {}
+    friend ostream& operator<<(ostream&, const Hint& h);
+};
+ostream& operator<<(ostream& o, const Hint& h) {
+
+}*/
 // void print_players(vector<Player>& players) {  //Print each payer and thier hand
 // for (int i = 0, len = players.size(); i < len; ++i)
 //     cout << players[i] << endl;
@@ -66,6 +101,26 @@ void print_players(vector<Player>& players) {  //Print each payer and thier hand
     for (int i = 0, len = players.size(); i < len; ++i)
         cout << players[i] << endl;
     cout << endl;
+}
+
+void winner(vector<Card>& hand) {
+/*for ( int i = 0, len = hand.size(); i < len; ++i) {
+		if( hand[i] == hand[i+1] ) {
+        cout << "True";
+    }
+    else {
+        cout << "False";
+    }
+	}
+}*/
+    if (equal(hand.begin() + 1, hand.end(), hand.begin()) ) {
+        cout << "True";
+    }
+    else if (!equal(hand.begin() + 1, hand.end(), hand.begin())) {
+        cout << "False" << endl; }
+    else {
+        cout << "False" << endl;
+    }
 }
 
 int main() {
@@ -94,14 +149,37 @@ int main() {
         }
     }
 
-    print_players(players);
+    vector<Card> player_hand;
     for (int i = 0, len = players.size(); i < len; ++i) {
-        cout << "Which one to replace? ";
-        cin >> user_input;
+        player_hand = players[0].get_hand();
+        //for (int j = 0, len = player_hand.size(); j < len; ++j) {
+                cout << player_hand[i] << endl;
+        //}
+    }
+    //winner(player_hand);
+    /*if (equal(player_hand.begin() + 1, player_hand.end(), player_hand.begin()) ) {
+    cout << "True";
+    }*/
+
+
+    while(true) {
+        for (int i = 0, len = players.size(); i < len; ++i) {
+            cout << "Which one to replace? ";
+            cin >> user_input;
+
+            if (user_input == '?') {
+                for (int i = 0, len = players.size(); i < len; ++i)
+                    cout << "Hint: " << players[i].get_name() << " passed "
+                        << "passed_card" << " to " << players[i + 1 ].get_name() << endl;
+                    cout << endl;
+                    continue;
+            }
+
         int index = find(players[i].letters.begin(), players[i].letters.end(), user_input) - players[i].letters.begin();
         Card passed_card = players[i].pass_card(index);
         players[(i+1) % players.size()].push_back(passed_card);
         print_players(players);
+    }
     }
 
     return 0;
