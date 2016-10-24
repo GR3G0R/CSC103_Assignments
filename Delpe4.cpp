7924b6018c9cb166120c1710760ef079f1bfc543
Gregory Delpe <gregory@namely.com>
2016-10-24 12:57:19 -0400

    Final submission - Assignment#4

--- Delpe4.cpp
+++ Delpe4.cpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 #include <algorithm>
+#include <random>
+#include <chrono>
+#include <iomanip>
 #include <cstdlib>
 #include <vector>
 using namespace std;
@@ -19,11 +22,14 @@ public:
     friend bool operator<(const Card& c1, const Card& c2); //Friend less-than operator
     char getCard() const { return m_char; }  //Access private member m_char trough member function getCard
     void push_ship(const bool& b) { deck.push_back(b); }
+    char getSuit(const Card& c) { return c.getCard(); }
+    int getNum() const { return m_num; }
+
 };
 ostream& operator<<(ostream& o, const Card& c) {  //Overload extraction operator to produce higher order cards
     o << c.m_char;
     switch (c.m_num) {
-    case 1: o << 'A'; break;
+    case 1: o  << 'A'; break;
     case 11: o << 'J'; break;
     case 12: o << 'K'; break;
     case 13: o << 'Q'; break;
@@ -33,22 +39,24 @@ ostream& operator<<(ostream& o, const Card& c) {  //Overload extraction operator
 }
 
 bool operator==(const Card& a, const Card& b) {  //Overload equality operator
-    return a.m_char == b.m_char;
-
+    //return a.m_char == b.m_char;
+    return a.m_num == b.m_num;
 }
 
 bool operator!=(const Card& c, const Card& d) { //Overload not equal to operator
-    return !(c.m_char == d.m_char);
+    //return !(c.m_char == d.m_char);
+    return !(c.m_num == d.m_num);
 }
 
 bool operator<(const Card& c1, const Card& c2) {  //Overload less-than operator
-    return c1.getCard() < c2.getCard();
+    //return c1.m_char < c2.m_char;
+    return c1.m_num < c2.m_num;
 }
 
 
 
 
-class Player {  //Introduce Player class
+class Player: public Card {  //Introduce Player class
 
     string m_name;
     vector<Card> m_deck;
@@ -56,26 +64,24 @@ public:
     vector<char> letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};  //Vector of deck elements
 
     Player(const string& n) : m_name(n) {m_deck.reserve(5); } //Player constructor
-    void push_back(const Card& c) {m_deck.push_back(c); }  //Define member function push_back - push cards onto player deck
-    void push_ship(const bool& b) {m_deck.push_back(b); }
+    void push_back(const Card& c) { m_deck.push_back(c); }  //Define member function push_back - push cards onto player deck
+    void push_ship(const bool& b) { m_deck.push_back(b); }
+    void push_char(const char& e) { m_deck.push_back(e); }
     friend ostream& operator<<(ostream&, const Player& p);  //Friend insertion operator
-    // Card get_card(int index) { return m_deck[index]; }
     friend bool operator==(const Player& c, const Player& d);  //Friend equality operator
     friend bool operator!=(const Player& a, const Player& b);  //Friend not equal to operator
+    friend bool operator<(const Player& c1, const Player& c2); //Friend less-than operator
+    char get_card(const Card& a) { return a.getCard(); }  //Access private member m_char trough member function getCard
     Card pass_card(int index) {  //Returns card to be removed and removes card from player deck
         Card result = m_deck[index];  //Assign returned card value to result variable
-        m_deck.erase(m_deck.begin() + index); return result;  //Remove element of chosen index, reduce container size, and shift position of remaining vectors
+        m_deck.erase(m_deck.begin() + index);  //Remove element of chosen index, reduce container size, and shift position of remaining vectors
+        return result;
     }
     string get_name() const { return m_name; }  //Access privat member m_name with member function
     vector<Card> get_hand() const { return m_deck; }  //Access private member m_deck with member function
+    char get_Suit(const Card& c) { return c.getCard(); }
 };
-// ostream& operator<<(ostream& o, const Player& p) {  //Extraction operator overloaded to print player name and hand
-//     o << "Player: " << p.m_name << " has ";
-//     for (int i = 0, len = p.m_deck.size(); i < len; ++i) {
-//         o << p.m_deck[i] << " ";
-//     }
-//     return o;
-// }
+
 ostream& operator<<(ostream& o, const Player& p) {  //Extraction operator overloaded to print player name and hand
     //o << p.m_name << " ";
     for (int i = 0, len = p.m_deck.size(); i < len; ++i) {
@@ -93,9 +99,14 @@ bool operator!=(const Player& c, const Player& d) {  //Overload not equal to ope
     return !(c == d);
 }
 
+bool operator<(const Player& c1, const Player& c2) {  //Overload less-than operator
+    return c1.get_hand() < c2.get_hand();
+}
+
+
 void print_players(vector<Player>& players) {  //Print each payer and thier hand
     for (int i = 0, len = players.size(); i < len; ++i)
-        cout << players[i] << endl;
+        cout << players[i].get_name() << " " << players[i] << endl;
     cout << endl;
 }
 
@@ -117,19 +128,92 @@ void finalDistro(vector<Player> players) {  //Print final distribution
     }
 }
 
+void passCard(vector<Card> hand, vector<Card>& suitCount , vector<Player>& players, int i, vector<Card>& pCards) {  //Function controls passing scheme and mac intelligence
+    Card cCard, sCard, hCard, dCard;
+    int sCount = 0, cCount = 0, dCount = 0, hCount =0;
+    int c_index, h_index, d_index, s_index;
+    for (int i = 0; i < 7; ++i) {
+        if (hand[i].getCard() == 'S') {  //Check for prsence of spade in player hand
+            ++sCount;  //increment spade counter if instance of spade discovered
+            sCard = hand[i];  //Copy card
+            s_index = i;  //Capy card index
+        }
+        if (hand[i].getCard() == 'H') {  //Chaeck for presence of hearts in player hand
+            ++hCount;  //increment heart counter if instance of heart disovered
+            hCard = hand[i];  //Copy Card
+            h_index = i; //Copy card index
+        }
+        if (hand[i].getCard() == 'C') {
+            ++cCount;
+            cCard = hand[i];
+            c_index = i;
+        }
+        if (hand[i].getCard() == 'D') {
+            ++dCount;
+            dCard = hand[i];
+            d_index = i;
+        }
+    }
+
+    //Push objects of card suit and count onto vector suitCount
+    if (sCount > 0) suitCount.push_back(Card('S', sCount));
+    if (hCount > 0) suitCount.push_back(Card('H', hCount));
+    if (dCount > 0) suitCount.push_back(Card('D', dCount));
+    if (cCount > 0) suitCount.push_back(Card('C', cCount));
+
+    Card least;
+    sort(suitCount.begin(), suitCount.end());  //Sort vector suitCount
+    for (int j = 0; j < suitCount.size(); ++j) {  //Identify least common suit
+        if (suitCount[j].getNum() == '1') {
+            least = suitCount[j];
+        }
+        else { least = suitCount.front(); }
+    }
+
+    if (least.getCard() == 'S') {  //Check for least common suit
+        Card player_pass = players[i].pass_card(s_index);  //Assign card of least common suit to pass variable
+        pCards.push_back(player_pass);  //Push card into pass card tracking vector
+        players[(i+1) % players.size()].push_back(player_pass);  //Pass card to next player and remove from hand
+    }
+    else if (least.getCard() == 'H') {
+        Card player_pass = players[i].pass_card(h_index);
+        pCards.push_back(player_pass);
+        players[(i+1) % players.size()].push_back(player_pass);
+    }
+    if (least.getCard() == 'D') {
+        Card player_pass = players[i].pass_card(d_index);
+        pCards.push_back(player_pass);
+        players[(i+1) % players.size()].push_back(player_pass);
+    }
+    else if (least.getCard() == 'C') {
+        Card player_pass = players[i].pass_card(c_index);
+        pCards.push_back(player_pass);
+        players[(i+1) % players.size()].push_back(player_pass);
+    }
+
+
+    suitCount.clear();  //Clear counter vector
+}
+
+
 int main() {
     vector<Player> players;
     char user_input;
-    vector<Card> playerHand, playerHand1, playerHand2, playerHand3, playerHand4;
+    vector<Card> playerHand, playerHand1, playerHand2, playerHand3, playerHand4, suitCount, pCards;
     vector<bool> shipStatus;
     vector<Card> test = {'a','a','a','a'};  //Test variable
+
+    for (int i = 0, len = suitCount.size(); i < len; ++i) { suitCount[i] = 0; }
 
     vector<Card> deck; deck.reserve(52);  //Declare vector deck
     for (int i = 1; i <= 13; ++i) deck.push_back(Card('S',i));  //push_back spades suit
     for (int i = 1; i <= 13; ++i) deck.push_back(Card('C',i));  //push_back clubs suit
     for (int i = 1; i <= 13; ++i) deck.push_back(Card('D',i));  //push_back diamonds suit
     for (int i = 1; i <= 13; ++i) deck.push_back(Card('H',i));  //push_back clubs suit
-    random_shuffle(deck.begin(), deck.end());  //Shuffle deck
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    shuffle(deck.begin(), deck.end(), std::default_random_engine(seed));  //Shuffle deck
+
 
     //Add contestants to vector players
     players.push_back(Player("Player 0"));
@@ -137,7 +221,7 @@ int main() {
     players.push_back(Player("Player 2"));
     players.push_back(Player("Player 3"));
     players.push_back(Player("Player 4"));
-    players.push_back(Player("Test"));  //Test player
+    //players.push_back(Player("Test"));  //Test player
 
 
 
@@ -156,41 +240,51 @@ int main() {
 
             cout << "Which one to replace? ";  //Promt user to pass unwanted card
             cin >> user_input;  //Get user input
+            cout << endl << endl;
 
             if (user_input == '?') {  //Hint - reveals all passed cards
                 print_players(players);  //Print each player and their respective hand
                 continue;
             }
 
-            int index = find(players[i].letters.begin(), players[i].letters.end(), user_input) - players[i].letters.begin();  //Define variable for index of passed card
-            Card passed_card = players[i].pass_card(index);  //variable passed_card is assigned returned card at index of card passed by user
+            int index = find(players[0].letters.begin(), players[0].letters.end(), user_input) - players[0].letters.begin();  //Define variable for index of passed card
+            Card passed_card = players[0].pass_card(index);  //variable passed_card is assigned returned card at index of card passed by user
 
-            for (int i = 0, len = players.size(); i < len; ++i)  //Produce user hint
-                cout << "Hint: " << players[i].get_name() << " passed "
-                    << passed_card << " to " << players[i + 1 ].get_name() << endl;
-                cout << endl;
+            pCards.push_back(passed_card);  //Push user slected pass card to tracking vector
 
-            players[(i+1) % players.size()].push_back(passed_card);  //Push passed card onto next players deck
+            players[(1) % players.size()].push_back(passed_card);  //Push passed card onto next players deck
 
             for (int i = 0, len = 7; i < len; ++i) {
-                playerHand = players[0].get_hand();  //Assign deck component of player to independent vector playerHand*
+                playerHand  = players[0].get_hand();  //Assign deck component of player to independent vector playerHand*
                 playerHand1 = players[1].get_hand();
                 playerHand2 = players[2].get_hand();
                 playerHand3 = players[3].get_hand();
                 playerHand4 = players[4].get_hand();
-                cout << playerHand[i] << endl << endl;  //Test - view independent playerHand
+               //cout << playerHand[i] << endl << endl;  //Test - view independent playerHand
             }
 
-            //Build vector of booleans for status of 
+            //void passCard(vector<Player> hand, vector<Card>& suitCount , vector<Player> players, int i) {
+            passCard(playerHand1, suitCount, players, 1, pCards);
+            passCard(playerHand2, suitCount, players, 2, pCards);
+            passCard(playerHand3, suitCount, players, 3, pCards);
+            passCard(playerHand4, suitCount, players, 4, pCards);
+
+            for (int i = 0, len = players.size(); i < len; ++i)  //Produce user hint
+                cout << "Hint: " << players[i].get_name() << " passed "
+                    << pCards[i] << setw(5) << left << " to "
+                    << right << players[(i+1) % players.size()].get_name() << endl;
+                cout << endl;
+
+            pCards.clear();  //Clear tracking vector
+
+            //Build vector of booleans for status of
             //players with/without sailed ship
             shipStatus.push_back(shipSails(playerHand));
             shipStatus.push_back(shipSails(playerHand1));
             shipStatus.push_back(shipSails(playerHand2));
             shipStatus.push_back(shipSails(playerHand3));
             shipStatus.push_back(shipSails(playerHand4));
-            shipStatus.push_back(shipSails(test));
-
-
+            //shipStatus.push_back(shipSails(test));  //Test shipSails method
 
             if (find(shipStatus.begin(), shipStatus.end(), true) != shipStatus.end()) {  //Search for any intance of sailed ship evaluated to 1
                 for (int i = 0, len = shipStatus.size(); i < len; ++i) {  //Iterate over shipStatus
@@ -202,11 +296,13 @@ int main() {
                 finalDistro(players);  //Print final distribution
                 return 0;  //End game
             }
+            else {
+                shipStatus.clear();  //Claer shipStatus
+            }
 
             for (int i = 0, len = shipStatus.size(); i < len; ++i) {  //Test - view vector shipStatus
                 cout << shipStatus[i] << endl << endl;
             }
-
         }
     }
 
